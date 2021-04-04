7a39294f8370c573337e9789231ce1908fc5e25c
PrashantRaj18198 <prashantraj18198@gmail.com>
2021-04-04 11:41:11 +0530

    add team table and user api token

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { withFrontegg } from "./withFrontegg";
+import { Link } from "react-router-dom";
+import {
+  Team,
+  useAuth,
+  useIsAuthenticated,
+  UserApiTokens,
+} from "@frontegg/react-auth";
 
 function App() {
+  const isAuthenticated = useIsAuthenticated();
+  const { user } = useAuth();
+  console.log({ authorization: `Bearer ${user?.accessToken}` });
   return (
     <div className="App">
       <header className="App-header">
@@ -17,9 +28,50 @@ function App() {
         >
           Learn React
         </a>
+        {console.log(user)}
+        {/* if user not authenticated show links to signup and login */}
+        {!isAuthenticated ? (
+          <>
+            Sign-up to argonaut-frontegg. <br></br>
+            <Link to={"/account/sign-up"}>
+              <button>Sign Up</button>
+            </Link>
+            <br></br>
+            Log-in to argonaut-frontegg.{" "}
+            <Link to={"/account/login"}>
+              <button>Login</button>
+            </Link>
+            <br></br>
+          </>
+        ) : (
+          <></>
+        )}
+
+        {/* if user authenticated show link to logout */}
+        {isAuthenticated ? (
+          <>
+            <Link to={"/account/logout"}>
+              <button>Logout</button>
+            </Link>
+          </>
+        ) : (
+          <></>
+        )}
       </header>
+      {isAuthenticated && (
+        <>
+          <div>
+            <UserApiTokens.Page />
+          </div>
+        </>
+      )}
+      {isAuthenticated && (
+        <div>
+          <Team.Page />
+        </div>
+      )}
     </div>
   );
 }
 
-export default App;
+export default withFrontegg(App);
